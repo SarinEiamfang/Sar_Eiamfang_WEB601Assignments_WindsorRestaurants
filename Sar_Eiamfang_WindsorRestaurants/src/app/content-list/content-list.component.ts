54f21bc6fcb58f2d9d2b4a8393c67b19e6b79769
SarinEiamfang <w0830970@myscc.ca>
2024-01-28 20:50:33 -0500

    Create new component Content-list

---
+++ Sar_Eiamfang_WindsorRestaurants/src/app/content-list/content-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-list.component.html',
+  styleUrl: './content-list.component.scss'
+})
+export class ContentListComponent {
+
+}

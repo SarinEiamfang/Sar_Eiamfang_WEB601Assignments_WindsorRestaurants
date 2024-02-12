d48235e768c299b68f80c058f7b0ca1548b0e5f6
SarinEiamfang <w0830970@myscc.ca>
2024-02-12 15:38:30 -0500

    apply effect

--- Sar_Eiamfang_WindsorRestaurants/src/app/hover-affect.directive.ts
+++ Sar_Eiamfang_WindsorRestaurants/src/app/hover-affect.directive.ts
@@ -1,11 +1,50 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]',
   standalone: true
 })
 export class HoverAffectDirective {
+  
+    @Input() affectType: 'underline' | 'bold' | 'border' = 'underline';
+  
+    constructor(private el: ElementRef, private renderer: Renderer2) { }
+    
+    @HostListener('mouseenter') onMouseEnter() {
+      this.applyEffect();
+    }
+  
+    @HostListener('mouseleave') onMouseLeave() {
+      this.removeEffect();
+    }
+  
+    private applyEffect() {
+      switch (this.affectType) {
+        case 'underline':
+          this.renderer.setStyle(this.el.nativeElement, 'text-decoration', 'underline');
+          break;
+        case 'bold':
+          this.renderer.setStyle(this.el.nativeElement, 'font-weight', 'bold');
+          break;
+        case 'border': // This effect doesn't work
+          this.renderer.setStyle(this.el.nativeElement.parentNode, 'border', '2px solid red');
+          break;
+      }
+    }
+  
+    private removeEffect() {
+      switch (this.affectType) {
+        case 'underline':
+          this.renderer.removeStyle(this.el.nativeElement, 'text-decoration');
+          break;
+        case 'bold':
+          this.renderer.removeStyle(this.el.nativeElement, 'font-weight');
+          break;
+        case 'border': // This effect doesn't work
+          this.renderer.removeStyle(this.el.nativeElement.parentNode, 'border');
+          break;
+      }
+    }
+  }
 
-  constructor() { }
 
-}

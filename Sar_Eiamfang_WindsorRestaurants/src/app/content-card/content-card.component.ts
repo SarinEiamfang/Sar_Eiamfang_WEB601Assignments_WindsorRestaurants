fa2f0c4a6485215ab1060d39f368a0aa82a0d522
SarinEiamfang <w0830970@myscc.ca>
2024-02-12 15:26:01 -0500

    generated directive hover-affect

--- Sar_Eiamfang_WindsorRestaurants/src/app/content-card/content-card.component.ts
+++ Sar_Eiamfang_WindsorRestaurants/src/app/content-card/content-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HoverAffectDirective } from '../hover-affect.directive';
 
 @Component({
   selector: 'app-content-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HoverAffectDirective],
   templateUrl: './content-card.component.html',
   styleUrl: './content-card.component.scss'
 })
---
+++ Sar_Eiamfang_WindsorRestaurants/src/app/hover-affect.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHoverAffect]',
+  standalone: true
+})
+export class HoverAffectDirective {
+
+  constructor() { }
+
+}

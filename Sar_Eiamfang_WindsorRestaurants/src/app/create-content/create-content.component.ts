5600c61ebaf0645db4ced6040395d232eb13cbed
SarinEiamfang <w0830970@myscc.ca>
2024-03-03 22:13:19 -0500

    generated new component CreateContent

---
+++ Sar_Eiamfang_WindsorRestaurants/src/app/create-content/create-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-content.component.html',
+  styleUrl: './create-content.component.scss'
+})
+export class CreateContentComponent {
+
+}
